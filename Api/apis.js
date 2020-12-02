bae022a4605a18d8153eaa6ed77a4da92a39613a
René Vandamme <vandammerene9+gitkraken@gmail.com>
2020-12-02 10:32:42 +0100

    Basic page layout.

---
+++ Api/apis.js
@@ -0,0 +1,46 @@
+//File containing a json obj called WEATHER_CONFIG with my api key.
+import { WEATHER_CONFIG, UNSPLASH_CONFIG } from "../config.js";
+
+const openWeatherMapAPI = axios.create({
+  baseURL: "https://api.openweathermap.org/data/2.5",
+});
+
+const openCageDataAPI = axios.create({
+  baseURL: "https://api.opencagedata.com/geocode/v1",
+});
+
+const unsplashAPI = axios.create({
+  baseURL: "https://api.unsplash.com/",
+  headers: {
+    Authorization: `Client-ID ${UNSPLASH_CONFIG.accessKey}`, //the token is a variable which holds the token
+  },
+});
+
+function getCurrentWeatherDataForLocation(location) {
+  return openWeatherMapAPI.get(
+    `/weather?q=${location}&units=metric&appid=${WEATHER_CONFIG.openWeatherMapApiKey}`
+  );
+}
+
+async function getAddressFromLatLng(lat, lng) {
+  //https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=API_KEY
+  var response = await openCageDataAPI.get(
+    `/json?q=${lat}+${lng}&key=${WEATHER_CONFIG.openCageDataAPiKey}`
+  );
+  //TODO: add some validation
+  return response.data.results[0];
+}
+
+async function getPictureforLocation(location) {
+  var response = await unsplashAPI.get(
+    `/search/photos?query=${location}&per_page=1&orientation=landscape`
+  );
+  //TODO: add some validation
+  return response.data.results[0].urls.raw;
+}
+
+export {
+  getCurrentWeatherDataForLocation,
+  getAddressFromLatLng,
+  getPictureforLocation,
+};
--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-import { getCurrentWeatherDataForLocation } from './Api/openweathermap.js';
+import {
+  getCurrentWeatherDataForLocation,
+  getAddressFromLatLng,
+  getPictureforLocation,
+} from "./Api/apis.js";
 (() => {
+  let cityInput = document.getElementById("city");
+
+  const setPageBackground = (imageUrl) => {
+    let bodyElement = document.getElementById("background");
+    bodyElement.style.backgroundImage = `url('${imageUrl}')`;
+  };
+
+  const requestUserLocation = () => {
+    //https://www.w3schools.com/html/html5_geolocation.asp
+    //Note, it's highly likely that a user might deny our site it's location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        onUserLocationRetrieved,
+        onUserLocationDenied
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  };
+
+  const onUserLocationRetrieved = async (position) => {
+    let response = await getAddressFromLatLng(
+      position.coords.latitude,
+      position.coords.longitude
+    );
+    cityInput.value = response.components.city;
+  };
+
+  const onUserLocationDenied = (error) => {
+    console.error(error);
+  };
+
   const onSubmit = async (event) => {
     event.preventDefault();
-    var location = document.getElementById('city').value;
-    var promise = await getCurrentWeatherDataForLocation(location);
-    console.log(promise.data);
+    var location = cityInput.value;
+    var weatherDataResponse = await getCurrentWeatherDataForLocation(location);
+    console.log(weatherDataResponse);
+    var pictureDataResponse = await getPictureforLocation(`${location} city`);
+    console.log(pictureDataResponse);
+    setPageBackground(pictureDataResponse);
   };
+
+  requestUserLocation();
   document
-    .getElementById('location-date-form')
-    .addEventListener('submit', onSubmit);
+    .getElementById("location-date-form")
+    .addEventListener("submit", onSubmit);
 })();
