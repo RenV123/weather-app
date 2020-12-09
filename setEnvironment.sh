0d907424998e232024f1441ea29a7a2152495a43
René Vandamme <vandammerene9+gitkraken@gmail.com>
2020-12-09 12:44:23 +0100

    Remove unused setenvironment shell script

--- setEnvironment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-{
-    echo -e "export var WEATHER_CONFIG = {"  
-    echo -e "  openWeatherMapApiKey: \"$1\",\n  openCageDataAPiKey: \"$2\",\n};"
-    echo -e "export var UNSPLASH_CONFIG = {"
-    echo -e "  clientId: \"$3\","
-    echo -e "  accessKey: \"$4\",\n};"  
-} >> config.js
